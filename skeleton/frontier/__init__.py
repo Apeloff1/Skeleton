0e1e46896c035eab1a4a741a92b6b9e60d16dfb7
Apeloff1 <122585327+Apeloff1@users.noreply.github.com>
2026-09-13 01:47:40 +0200

    feat: add frontier consolidation package

---
+++ skeleton/frontier/__init__.py
@@ -0,0 +1,5 @@
+"""Frontier consolidation contracts and program metadata."""
+
+from skeleton.frontier.contracts import AgentContract, MemoryContract, ProvenanceRecord
+
+__all__ = ["AgentContract", "MemoryContract", "ProvenanceRecord"]
