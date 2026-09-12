4075b614ea071126c54a77d0f9c46eb8c08f5cc2
Apeloff1 <122585327+Apeloff1@users.noreply.github.com>
2026-09-12 20:49:13 +0200

    feat(school): export debug CS and energy policies

--- skeleton/school/__init__.py
+++ skeleton/school/__init__.py
@@ -5,7 +5,10 @@ from skeleton.school.code_intelligence import CodeIntelligenceEngine, CodeIntell
 from skeleton.school.code_lab import CodeFinding, CodeLabEngine, CodeReview, CodeTask, FindingSeverity
 from skeleton.school.cocoding import CodingPhase, CoCodingAction, CoCodingContext, HandoffStage, InteractionPattern, choose_action, next_handoff
 from skeleton.school.curriculum import CurriculumGraph, CurriculumNode, LearningRecommendation, rank_recommendations
+from skeleton.school.debugging import DebugAction, DebugExperiment, DebugFocus, DebugHypothesis, DebugPlan, DebuggingPolicy
+from skeleton.school.energy import EnergyBudget, EnergyDecision, EnergyStrategy, choose_energy_strategy
 from skeleton.school.engine import SchoolEngine, SchoolPlan
+from skeleton.school.cs_pathways import CSFamily, CSPathway, PATHWAYS, next_pathway, pathways_for
 from skeleton.school.learning_control import LearningControl, LearningControlDecision, LearningState
 from skeleton.school.lesson_content import LessonContent, LessonExercise, LessonTopic, generate_lesson_content
 from skeleton.school.memory import LearnerMemory, MemoryKind, MemoryMatch, MemoryStore
@@ -21,7 +24,10 @@ __all__ = [
     "CodingPhase", "CoCodingAction", "CoCodingContext", "HandoffStage", "InteractionPattern",
     "choose_action", "next_handoff",
     "CurriculumGraph", "CurriculumNode", "LearningRecommendation", "rank_recommendations",
-    "SchoolEngine", "SchoolPlan", "LearningControl", "LearningControlDecision", "LearningState",
+    "DebugAction", "DebugExperiment", "DebugFocus", "DebugHypothesis", "DebugPlan", "DebuggingPolicy",
+    "EnergyBudget", "EnergyDecision", "EnergyStrategy", "choose_energy_strategy",
+    "SchoolEngine", "SchoolPlan", "CSFamily", "CSPathway", "PATHWAYS", "next_pathway", "pathways_for",
+    "LearningControl", "LearningControlDecision", "LearningState",
     "LessonContent", "LessonExercise", "LessonTopic", "generate_lesson_content",
     "LearnerMemory", "MemoryKind", "MemoryMatch", "MemoryStore",
     "AchievementEvidence", "AchievementRequirement", "AchievementResult", "ProgressionSnapshot", "achievement_learning_signal", "evaluate_achievement", "evaluate_progression",
