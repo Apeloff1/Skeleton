da9a6d3377111feb65e003286cfe0a0a4e95be64
Apeloff1 <122585327+Apeloff1@users.noreply.github.com>
2026-09-12 20:32:46 +0200

    feat(school): export code intelligence engine

--- skeleton/school/__init__.py
+++ skeleton/school/__init__.py
@@ -1,7 +1,8 @@
 """School primitives for the Tutolage + Jeeves learning core."""
 
 from skeleton.school.assessment import AssessmentEngine, AssessmentEvidence, AssessmentKind, Intervention
-from skeleton.school.code_lab import CodeFinding, CodeLabEngine, CodeReview, CodeTask, FindingSeverity
+from skeleton.school.code_intelligence import CodeIntelligenceEngine, CodeIntelligenceReport, CodeSignal, CodeSignalKind, CodeTask, CodeTaskKind
+from skeleton.school.code_lab import CodeFinding, CodeLabEngine, CodeReview, CodeTask as LabCodeTask, FindingSeverity
 from skeleton.school.cocoding import CodingPhase, CoCodingAction, CoCodingContext, HandoffStage, InteractionPattern, choose_action, next_handoff
 from skeleton.school.curriculum import CurriculumGraph, CurriculumNode, LearningRecommendation, rank_recommendations
 from skeleton.school.engine import SchoolEngine, SchoolPlan
@@ -12,7 +13,8 @@ from skeleton.school.student import SkillState, StudentProfile
 
 __all__ = [
     "AssessmentEngine", "AssessmentEvidence", "AssessmentKind", "Intervention",
-    "CodeFinding", "CodeLabEngine", "CodeReview", "CodeTask", "FindingSeverity",
+    "CodeIntelligenceEngine", "CodeIntelligenceReport", "CodeSignal", "CodeSignalKind", "CodeTask", "CodeTaskKind",
+    "CodeFinding", "CodeLabEngine", "CodeReview", "LabCodeTask", "FindingSeverity",
     "CodingPhase", "CoCodingAction", "CoCodingContext", "HandoffStage", "InteractionPattern",
     "choose_action", "next_handoff",
     "CurriculumGraph", "CurriculumNode", "LearningRecommendation", "rank_recommendations",
