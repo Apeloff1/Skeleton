0ada76e8b5e4bd02dfb8c1af94fee579ed39cb91
Apeloff1 <122585327+Apeloff1@users.noreply.github.com>
2026-09-13 02:43:45 +0200

    test receipt-aware runtime admission

--- tests/test_gameforge_runtime_contract.py
+++ tests/test_gameforge_runtime_contract.py
@@ -3,8 +3,8 @@ from skeleton.frontier.gameforge_dependency import DependencyGate
 from skeleton.frontier.gameforge_lifecycle import ServiceLifecycle
 from skeleton.frontier.gameforge_runtime_contract import RuntimeContract
 
-def test_runtime_contract_composes_readiness_and_admission():
- s=ServiceLifecycle(); d=DependencyGate(("core",)); r=RuntimeContract(s,d); assert r.admit() is Admission.SHED; s.ready(); assert r.admit() is Admission.SHED; d.mark("core"); assert r.admit() is Admission.ACCEPT
+def test_runtime_contract_returns_observable_receipt():
+ s=ServiceLifecycle(); d=DependencyGate(("core",)); r=RuntimeContract(s,d); assert r.admit("r1").decision==Admission.SHED.value; s.ready(); d.mark("core"); assert r.admit("r1").accepted()
 
-def test_runtime_contract_can_enter_read_only():
- s=ServiceLifecycle(); d=DependencyGate(("core",)); r=RuntimeContract(s,d); s.ready(); d.mark("core"); assert r.admit(read_only=True) is Admission.READ_ONLY
+def test_runtime_contract_preserves_read_only():
+ s=ServiceLifecycle(); d=DependencyGate(("core",)); r=RuntimeContract(s,d); s.ready(); d.mark("core"); assert r.admit("r1",read_only=True).decision==Admission.READ_ONLY.value
