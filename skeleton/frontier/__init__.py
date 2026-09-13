b08d4fcc6b2ac846e76f9c9a6d554d99ee697a5f
Apeloff1 <122585327+Apeloff1@users.noreply.github.com>
2026-09-13 11:48:10 +0200

    feat(frontier): expose promoted runtime primitives

--- skeleton/frontier/__init__.py
+++ skeleton/frontier/__init__.py
@@ -4,10 +4,14 @@ from skeleton.frontier.agent_runtime import AgentRuntime, ExecutionResult
 from skeleton.frontier.contracts import AgentContract, MemoryContract, ProvenanceRecord
 from skeleton.frontier.memory import InMemoryStore, MemoryItem
 from skeleton.frontier.npc import NPCGenerator, NPCSpec
+from skeleton.frontier.npc_profiles import ArchetypeProfile, get_profile, infer_archetype
+from skeleton.frontier.resilience import DegradationLevel, ResilienceController, ResilienceDecision
 
 __all__ = [
     "AgentContract",
     "AgentRuntime",
+    "ArchetypeProfile",
+    "DegradationLevel",
     "ExecutionResult",
     "InMemoryStore",
     "MemoryContract",
@@ -15,4 +19,8 @@ __all__ = [
     "NPCGenerator",
     "NPCSpec",
     "ProvenanceRecord",
+    "ResilienceController",
+    "ResilienceDecision",
+    "get_profile",
+    "infer_archetype",
 ]
