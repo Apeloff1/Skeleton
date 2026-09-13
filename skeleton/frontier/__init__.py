a0ac8e2636da56246c186a67a9b252de5992383e
Apeloff1 <122585327+Apeloff1@users.noreply.github.com>
2026-09-13 11:55:12 +0200

    refactor(frontier): expose policy and health primitives

--- skeleton/frontier/__init__.py
+++ skeleton/frontier/__init__.py
@@ -1,8 +1,10 @@
 """Frontier consolidation contracts and program metadata."""
 
 from skeleton.frontier.agent_runtime import AgentRuntime, ExecutionResult
+from skeleton.frontier.capabilities import CapabilityPolicy
 from skeleton.frontier.contracts import AgentContract, MemoryContract, ProvenanceRecord
 from skeleton.frontier.events import DomainEvent, EventBus
+from skeleton.frontier.health import HealthReport, HealthState, evaluate_health
 from skeleton.frontier.memory import InMemoryStore, MemoryItem
 from skeleton.frontier.npc import NPCGenerator, NPCSpec
 from skeleton.frontier.npc_profiles import ArchetypeProfile, get_profile, infer_archetype
@@ -12,10 +14,13 @@ __all__ = [
     "AgentContract",
     "AgentRuntime",
     "ArchetypeProfile",
+    "CapabilityPolicy",
     "DegradationLevel",
     "DomainEvent",
     "EventBus",
     "ExecutionResult",
+    "HealthReport",
+    "HealthState",
     "InMemoryStore",
     "MemoryContract",
     "MemoryItem",
@@ -24,6 +29,7 @@ __all__ = [
     "ProvenanceRecord",
     "ResilienceController",
     "ResilienceDecision",
+    "evaluate_health",
     "get_profile",
     "infer_archetype",
 ]
