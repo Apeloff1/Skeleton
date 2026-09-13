8fa22b3454727e4c8da45b2ec9976541bd6a4cf3
Apeloff1 <122585327+Apeloff1@users.noreply.github.com>
2026-09-13 02:40:56 +0200

    test: cover runtime read-only admission

--- tests/test_gameforge_runtime_contract.py
+++ tests/test_gameforge_runtime_contract.py
@@ -5,3 +5,6 @@ from skeleton.frontier.gameforge_runtime_contract import RuntimeContract
 
 def test_runtime_contract_composes_readiness_and_admission():
  s=ServiceLifecycle(); d=DependencyGate(("core",)); r=RuntimeContract(s,d); assert r.admit() is Admission.SHED; s.ready(); assert r.admit() is Admission.SHED; d.mark("core"); assert r.admit() is Admission.ACCEPT
+
+def test_runtime_contract_can_enter_read_only():
+ s=ServiceLifecycle(); d=DependencyGate(("core",)); r=RuntimeContract(s,d); s.ready(); d.mark("core"); assert r.admit(read_only=True) is Admission.READ_ONLY
