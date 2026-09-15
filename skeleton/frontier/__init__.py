cf494fdb380b0a0d0d1ffdf72ea12dd56a025963
Apeloff1 <122585327+Apeloff1@users.noreply.github.com>
2026-09-15 11:27:58 +0200

    frontier: export integration wave adapters

--- skeleton/frontier/__init__.py
+++ skeleton/frontier/__init__.py
@@ -1,10 +1,17 @@
 """Frontier consolidation contracts and program metadata."""
 
 from skeleton.frontier.agent_runtime import AgentRuntime, ExecutionResult
-from skeleton.frontier.contracts import AgentContract, MemoryContract, ProvenanceRecord
+from skeleton.frontier.contracts import (
+    AgentContract,
+    MemoryContract,
+    ProvenanceRecord,
+    stable_content_digest,
+)
 from skeleton.frontier.events import DomainEvent, EventBus
 from skeleton.frontier.memory import InMemoryStore, MemoryItem
+from skeleton.frontier.memory_adapters import CollectionMemoryAdapter
 from skeleton.frontier.npc import NPCGenerator, NPCSpec
+from skeleton.frontier.npc_adapters import npc_spec_from_domain_record
 from skeleton.frontier.npc_profiles import ArchetypeProfile, get_profile, infer_archetype
 from skeleton.frontier.resilience import DegradationLevel, ResilienceController, ResilienceDecision
 
@@ -12,6 +19,7 @@ __all__ = [
     "AgentContract",
     "AgentRuntime",
     "ArchetypeProfile",
+    "CollectionMemoryAdapter",
     "DegradationLevel",
     "DomainEvent",
     "EventBus",
@@ -26,4 +34,6 @@ __all__ = [
     "ResilienceDecision",
     "get_profile",
     "infer_archetype",
+    "npc_spec_from_domain_record",
+    "stable_content_digest",
 ]
