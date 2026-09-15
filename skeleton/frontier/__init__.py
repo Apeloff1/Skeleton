7887f24ad83a163b16d235dc5d40f71121bce5d1
Apeloff1 <122585327+Apeloff1@users.noreply.github.com>
2026-09-15 11:34:28 +0200

    frontier: export sqlite memory backend

--- skeleton/frontier/__init__.py
+++ skeleton/frontier/__init__.py
@@ -9,7 +9,7 @@ from skeleton.frontier.contracts import (
 )
 from skeleton.frontier.events import DomainEvent, EventBus
 from skeleton.frontier.memory import InMemoryStore, MemoryItem
-from skeleton.frontier.memory_adapters import CollectionMemoryAdapter
+from skeleton.frontier.memory_adapters import CollectionMemoryAdapter, SQLiteCollection
 from skeleton.frontier.npc import NPCGenerator, NPCSpec
 from skeleton.frontier.npc_adapters import npc_spec_from_domain_record
 from skeleton.frontier.npc_profiles import ArchetypeProfile, get_profile, infer_archetype
@@ -32,6 +32,7 @@ __all__ = [
     "ProvenanceRecord",
     "ResilienceController",
     "ResilienceDecision",
+    "SQLiteCollection",
     "get_profile",
     "infer_archetype",
     "npc_spec_from_domain_record",
