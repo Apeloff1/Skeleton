6bd138822c9ddd1ff34119333564c3b11fb1af62
Apeloff1 <122585327+Apeloff1@users.noreply.github.com>
2026-09-16 04:45:56 +0200

    feat(gaming): export game knowledge retrieval

--- skeleton/acquired/gaming/__init__.py
+++ skeleton/acquired/gaming/__init__.py
@@ -8,12 +8,22 @@ from skeleton.acquired.gaming.course_runtime import (
     orient_ring_path,
     segment_crosses_ring,
 )
+from skeleton.acquired.gaming.knowledge import (
+    GameKnowledgeBase,
+    GameReference,
+    ScoredGameReference,
+    build_game_knowledge_context,
+)
 
 __all__ = [
     "CourseTracker",
+    "GameKnowledgeBase",
+    "GameReference",
     "GateCrossing",
     "Point3",
     "RingGate",
+    "ScoredGameReference",
+    "build_game_knowledge_context",
     "orient_ring_path",
     "segment_crosses_ring",
 ]
