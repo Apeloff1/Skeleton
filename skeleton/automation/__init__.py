4f7e984706c226c616f19a27e91c5f6676ab4626
Apeloff1 <122585327+Apeloff1@users.noreply.github.com>
2026-09-16 13:06:02 +0200

    refactor(backlog): export repository reference records

--- skeleton/automation/__init__.py
+++ skeleton/automation/__init__.py
@@ -1,12 +1,19 @@
 """Repository backlog automation primitives."""
 
-from .backlog_index import DependencyRecord, RepositoryIndex, RepositoryIndexBuilder, SymbolRecord
+from .backlog_index import (
+    DependencyRecord,
+    ReferenceRecord,
+    RepositoryIndex,
+    RepositoryIndexBuilder,
+    SymbolRecord,
+)
 from .backlog_reader import Document, Index, RepositoryReader
 
 __all__ = [
     "DependencyRecord",
     "Document",
     "Index",
+    "ReferenceRecord",
     "RepositoryIndex",
     "RepositoryIndexBuilder",
     "RepositoryReader",
