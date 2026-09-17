b4197e052b9a6dd55843183024b147b757af2f79
Apeloff1 <122585327+Apeloff1@users.noreply.github.com>
2026-09-18 01:22:36 +0200

    test(physics): register typed constraint suite in canonical runner

--- tests/run_unit.py
+++ tests/run_unit.py
@@ -38,6 +38,7 @@ TEST_MODULE_NAMES = (
     "tests.test_cortex",
     "tests.test_cortex_acquire_contract",
     "skeleton.testing.test_simulation_physics_ccd_advanced",
+    "skeleton.testing.test_simulation_physics_constraints",
     "skeleton.testing.test_simulation_physics_foundation",
     "skeleton.testing.test_simulation_physics_islands",
     "skeleton.testing.test_simulation_physics_manifolds",
