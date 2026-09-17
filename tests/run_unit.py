ef0acc093217d584549ab4b56f814e44ec874c12
Apeloff1 <122585327+Apeloff1@users.noreply.github.com>
2026-09-18 01:15:12 +0200

    test(physics): register island regressions in canonical runner

--- tests/run_unit.py
+++ tests/run_unit.py
@@ -39,6 +39,7 @@ TEST_MODULE_NAMES = (
     "tests.test_cortex_acquire_contract",
     "skeleton.testing.test_simulation_physics_ccd_advanced",
     "skeleton.testing.test_simulation_physics_foundation",
+    "skeleton.testing.test_simulation_physics_islands",
     "skeleton.testing.test_simulation_physics_manifolds",
     "skeleton.testing.test_simulation_physics_replay",
 )
