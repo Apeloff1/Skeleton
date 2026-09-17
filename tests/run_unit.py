0ba99ccc11eac06eef5d8713432bf42ab9e0cb49
Apeloff1 <122585327+Apeloff1@users.noreply.github.com>
2026-09-18 01:54:13 +0200

    test(physics): register convex collision suite in canonical runner

--- tests/run_unit.py
+++ tests/run_unit.py
@@ -40,6 +40,7 @@ TEST_MODULE_NAMES = (
     "skeleton.testing.test_simulation_physics_ccd_advanced",
     "skeleton.testing.test_simulation_physics_constraints",
     "skeleton.testing.test_simulation_physics_convex",
+    "skeleton.testing.test_simulation_physics_convex_collision",
     "skeleton.testing.test_simulation_physics_fixed_slider",
     "skeleton.testing.test_simulation_physics_foundation",
     "skeleton.testing.test_simulation_physics_hinge",
