a84612ed7f8a87dda3bfc53baeae52ec5d4cb328
Apeloff1 <122585327+Apeloff1@users.noreply.github.com>
2026-09-18 00:28:31 +0200

    feat(physics): expose deterministic physics api

---
+++ skeleton/simulation/physics/__init__.py
@@ -0,0 +1,73 @@
+"""Deterministic engine-neutral 3D physics foundation."""
+
+from .body import BodyType, RigidBody
+from .collision import (
+    BroadPhasePair,
+    ContactManifold,
+    ContactPoint,
+    SweepAndPruneBroadPhase,
+    detect_collision,
+    generate_manifolds,
+)
+from .errors import (
+    BodyNotFoundError,
+    DegenerateGeometryError,
+    DuplicateBodyError,
+    PhysicsError,
+    PhysicsValidationError,
+    SolverError,
+    UnsupportedCollisionError,
+)
+from .gameplay import (
+    GamePhysicsProfile,
+    GameplayScale,
+    JumpTuning,
+    ProjectileSolution,
+)
+from .materials import CombineRule, ContactMaterial, PhysicsMaterial, combine_materials
+from .math3d import AABB, Mat3, Quat, Transform, Vec3
+from .shapes import BoxShape, CollisionShape, MassProperties, PlaneShape, ShapeKind, SphereShape
+from .solver import SequentialImpulseSolver, SolverStats
+from .world import PhysicsSettings, PhysicsStepReceipt, PhysicsWorld
+
+__all__ = [
+    "AABB",
+    "BodyNotFoundError",
+    "BodyType",
+    "BoxShape",
+    "BroadPhasePair",
+    "CollisionShape",
+    "CombineRule",
+    "ContactManifold",
+    "ContactMaterial",
+    "ContactPoint",
+    "DegenerateGeometryError",
+    "DuplicateBodyError",
+    "GamePhysicsProfile",
+    "GameplayScale",
+    "JumpTuning",
+    "MassProperties",
+    "Mat3",
+    "PhysicsError",
+    "PhysicsMaterial",
+    "PhysicsSettings",
+    "PhysicsStepReceipt",
+    "PhysicsValidationError",
+    "PhysicsWorld",
+    "PlaneShape",
+    "ProjectileSolution",
+    "Quat",
+    "RigidBody",
+    "SequentialImpulseSolver",
+    "ShapeKind",
+    "SolverError",
+    "SolverStats",
+    "SphereShape",
+    "SweepAndPruneBroadPhase",
+    "Transform",
+    "UnsupportedCollisionError",
+    "Vec3",
+    "combine_materials",
+    "detect_collision",
+    "generate_manifolds",
+]
