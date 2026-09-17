c0352ae381e53ba9a133b9b4ea75417612605f26
Apeloff1 <122585327+Apeloff1@users.noreply.github.com>
2026-09-18 01:12:23 +0200

    test(physics): register advanced CCD suite in canonical runner

--- tests/run_unit.py
+++ tests/run_unit.py
@@ -37,6 +37,7 @@ TEST_MODULE_NAMES = (
     "tests.test_context",
     "tests.test_cortex",
     "tests.test_cortex_acquire_contract",
+    "skeleton.testing.test_simulation_physics_ccd_advanced",
     "skeleton.testing.test_simulation_physics_foundation",
     "skeleton.testing.test_simulation_physics_manifolds",
     "skeleton.testing.test_simulation_physics_replay",
