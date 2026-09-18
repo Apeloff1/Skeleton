b8e0c97f16d766e7636cea870c5a36fd706dd92b
Apeloff1 <122585327+Apeloff1@users.noreply.github.com>
2026-09-18 19:25:55 +0200

    feat(jeeves): export governed semantic plane runtime

--- skeleton/jeeves/agent/__init__.py
+++ skeleton/jeeves/agent/__init__.py
@@ -387,6 +387,25 @@ from .semantic_research_lenses import (
     research_semantic_definitions,
     research_semantic_specs,
 )
+from .semantic_plane_lenses import (
+    PLANE_CATALOG_VERSION,
+    plane_catalog_fingerprint,
+    plane_definitions_by_family,
+    plane_semantic_definitions,
+    plane_semantic_specs,
+    register_plane_lenses,
+)
+from .semantic_plane_interactions import (
+    plane_interaction_keys,
+    plane_interaction_rules,
+)
+from .semantic_governance_bridge import (
+    SemanticGovernanceBridge,
+    SemanticGovernanceSnapshot,
+    governed_family,
+    semantic_maturity_index,
+    semantic_spec_to_definition,
+)
 
 from .semantic_deep_lenses import DeepLensLineage, deep_lens_lineage, deep_semantic_lenses
 from .semantic_frontier import (
@@ -405,6 +424,14 @@ from .semantic_maximal import (
     MaximalSemanticRegistry,
     MaximalSemanticRuntime,
     MaximalSemanticSnapshot,
+)
+from .semantic_plane import (
+    FindingRejection,
+    SemanticFindingAudit,
+    SemanticLensPlane,
+    SemanticPlaneCoverage,
+    SemanticPlanePolicy,
+    SemanticPlaneSnapshot,
 )
 from .semantic_lenses import (
     JuxtapositionAnalyzer,
