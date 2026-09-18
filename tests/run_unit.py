f381e3a50d25c5af0e6eeb37a580aa6aaa6a3833
Apeloff1 <122585327+Apeloff1@users.noreply.github.com>
2026-09-18 02:29:35 +0200

    test(physics): register convex TOI suite in canonical runner

--- tests/run_unit.py
+++ tests/run_unit.py
@@ -42,6 +42,7 @@ TEST_MODULE_NAMES = (
     "skeleton.testing.test_simulation_physics_convex",
     "skeleton.testing.test_simulation_physics_convex_collision",
     "skeleton.testing.test_simulation_physics_convex_queries",
+    "skeleton.testing.test_simulation_physics_convex_toi",
     "skeleton.testing.test_simulation_physics_fixed_slider",
     "skeleton.testing.test_simulation_physics_foundation",
     "skeleton.testing.test_simulation_physics_hinge",
