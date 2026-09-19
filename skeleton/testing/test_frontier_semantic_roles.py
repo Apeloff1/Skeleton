1900f4260f8d0241a89b9aa0f6e98ce7193239c6
Apeloff1 <122585327+Apeloff1@users.noreply.github.com>
2026-09-19 08:48:26 +0200

    revert(jeeves): keep GB-8b scoped to artifact plane

--- skeleton/testing/test_frontier_semantic_roles.py
+++
@@ -1,7 +0,0 @@
-from skeleton.jeeves.agent.semantic_lenses import SemanticRole
-
-
-def test_frontier_semantic_catalog_roles_are_import_safe() -> None:
-    assert SemanticRole.SYSTEM.value == "system"
-    assert SemanticRole.SOCIAL.value == "social"
-    assert SemanticRole.TEMPORAL.value == "temporal"
