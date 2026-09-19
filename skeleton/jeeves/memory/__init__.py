a0104b9e7e37e2eb34490baa890ea99eeeb42681
Apeloff1 <122585327+Apeloff1@users.noreply.github.com>
2026-09-19 21:28:47 +0200

    feat(jeeves): expose memory assurance primitives

---
+++ skeleton/jeeves/memory/__init__.py
@@ -0,0 +1,4 @@
+"""Jeeves memory assurance surface."""
+from .index import MemoryEntry, MemoryIndex, MemoryTier
+from .policy import MemPolicyEntry, MemPolicyIndex, MemPolicyTier
+from .rehydration import RehydEntry, RehydIndex, RehydTier
